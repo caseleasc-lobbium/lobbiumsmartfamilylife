46a07e0604802f59a4fdc440961edabb0116cef1
CaselLobbium <info@lobbium.com>
2025-11-02 19:14:54 +0100

    Fix maintenance mode syntax

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,25 +1,6 @@
-import Head from "next/head";
 import "../styles/globals.css";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Maintenance from "./maintenance";
 
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Lobbium – Smart Family Life</title>
-        <meta
-          name="description"
-          content="Clever sparen, Alltag organisieren und Kinder spielerisch fördern."
-        />
-        <link rel="icon" href="/favicon.png" type="image/png" />
-      </Head>
-
-      <Header />
-      <main className="min-h-screen bg-white text-slate-800">
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </>
-  );
+export default function App() {
+  return <Maintenance />;
 }
